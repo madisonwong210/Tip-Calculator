4d0e11fb146f6081974a3f53868236645f26ae6c
madisonwong210 <58918746+madisonwong210@users.noreply.github.com>
2020-04-12 16:51:19 -0700

    Initial Commit

---
+++ tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tip
+//
+//  Created by Madison Wong on 4/12/20.
+//  Copyright © 2020 Madison Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
